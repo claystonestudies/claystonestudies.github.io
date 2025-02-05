7cae050dcdc4672add0fd69490c1c79c4671a2ea
claystonestudies <claystonestudies@gmail.com>
2025-02-05 08:39:53 -0500

    hamburger menu add

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
         yearElement.textContent = new Date().getFullYear();
     }
 });
+
+//hamburger menu below//
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
